1b4cde9d3fba2212bfbea398bf2392d7a0f536ab
suchithnarayan <suchithnarayan@gmail.com>
2020-11-27 17:54:54 +0530

    Reduced False Positives
    Pattern Matching

--- cve.py
+++ cve.py
@@ -4,18 +4,27 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+import re
 
 vuln = sys.argv[1]
-
 url = "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword="+vuln
 res = requests.get(url)
 soup = BeautifulSoup(res.content,'html.parser')
-td = soup.find_all('td', valign="top", nowrap="nowrap")
+td = soup.find_all('td', valign="top")
+#0,2,4 = cve | 1,3,5 = Description
 
 for i in td:
-	soup1 = BeautifulSoup(str(i),'html.parser')
-	try:
-		cves= soup1.find('a').get_text()
-	except:
+	index = td.index(i)
+	if index % 2 == 0:
 		pass
-	print(cves)
+	else:
+		soup1 = BeautifulSoup(str(i),'html.parser')
+		try:
+			desc = soup1.find('td').get_text()
+			reg = re.search(vuln.split()[0],desc,re.IGNORECASE)
+			if reg:
+				soup2 = BeautifulSoup(str(td[index-1]),'html.parser')
+				cves= soup2.find('a').get_text()
+				print(cves)
+		except:
+			pass
